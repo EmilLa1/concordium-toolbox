1878bd5c0b08ad471b25a58fcac045e1a38d237a
Emil Lai <el@concordium.com>
2022-05-10 23:32:14 +0200

    foo

--- chain/src/main.rs
+++ chain/src/main.rs
@@ -21,16 +21,42 @@ use tui::{
 #[derive(StructOpt)]
 struct Config {
     #[structopt(
-        long = "num_nodes",
+        long = "num-nodes",
         help = "The number of nodes to spawn",
         default_value = "5"
     )]
     num_nodes: usize,
     #[structopt(
-        long = "well_connected",
+        long = "well-connected",
         help = "If every node should be connected to eachother or they should be connected sequentially"
     )]
-    well_connected: bool,
+    optimal_connected: bool,
+    #[structopt(
+        long = "genesis-root",
+        help = "Path to genesis_data",
+        default_value = "../deps/concordium-node/scripts/genesis/genesis_data/"
+    )]
+    genesis_root: String,
+    #[structopt(
+        long = "rpc-port-offset",
+        help = "gRPC port offset. The nodes will be spawned this port and incrementing the port number for each",
+        default_value = "7000"
+    )]
+    rpc_port_offset: usize,
+    #[structopt(
+        long = "p2p-port-offset",
+        help = "P2p port offset. The nodes will be spawned this port and incrementing the port number for each",
+        default_value = "8000"
+    )]
+    peer_port_offset: usize,
+    #[structopt(long = "rts-flags", help = "RTS flags", default_value = "-N2")]
+    rts_flags: String,
+    #[structopt(
+        long = "housekeeping-interval",
+        help = "Interval in seconds where the node cleans up its connections etc.",
+        default_value = "30"
+    )]
+    housekeeping_interval: usize,
 }
 
 struct App<'a> {
@@ -39,9 +65,9 @@ struct App<'a> {
 }
 
 impl<'a> App<'a> {
-    fn new() -> App<'a> {
+    fn new(titles: &'a [std::string::String]) -> App<'a> {
         App {
-            titles: vec!["Node0", "Node1", "Node2", "Node3", "Node4"],
+            titles: titles.iter().map(AsRef::as_ref).collect(),
             index: 0,
         }
     }
@@ -74,8 +100,13 @@ async fn main() -> anyhow::Result<()> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let mut titles: Vec<String> = vec![];
+    for i in 0..cfg.num_nodes {
+        titles.push(format!("Node {:?}", i));
+    }
+
     // create app and run it
-    let app = App::new();
+    let app = App::new(&titles);
     let res = run_app(&mut terminal, app, &cfg);
 
     // restore terminal
@@ -100,18 +131,104 @@ fn run_app<B: Backend>(
     cfg: &Config,
 ) -> anyhow::Result<()> {
     // start the nodes.
+    let mut forks = vec![];
     let mut stdout_receivers = vec![];
     let mut log_buffers = vec![];
+    let genesis_root = std::path::PathBuf::from(&cfg.genesis_root)
+        .canonicalize()
+        .context("invalid genesis path")?;
+    let node_path = std::path::PathBuf::from("../deps/concordium-node/concordium-node/")
+        .canonicalize()
+        .context("invalid node path")?;
     for i in 0..cfg.num_nodes {
         log_buffers.push(String::new());
-        let mut fork = Command::new("./run-node-local.sh")
-            .arg(i.to_string())
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
+
+        // command for creating the baker folder
+        let mut mkdir_cmd = Command::new("mkdir");
+        mkdir_cmd.current_dir(&node_path);
+        mkdir_cmd.arg("-p");
+        mkdir_cmd.arg(format!("baker-{}", i));
+        mkdir_cmd
+            .status()
+            .context("cannot create baker directory")?;
+
+        //copy genesis.dat to baker directory cp $GENESIS_ROOT/genesis.dat
+        let mut copy_cmd = Command::new("cp");
+        copy_cmd.current_dir(&node_path);
+
+        let genesis_dat = genesis_root
+            .join("genesis.dat")
+            .canonicalize()
+            .context("cannot find genesis.dat")?;
+        copy_cmd.arg(genesis_dat.to_str().unwrap());
+        copy_cmd.arg(format!("baker-{}", i));
+        copy_cmd.status().context("cannot copy genesis.dat")?;
+
+        // command for running the node
+        let cmd = &mut Command::new("cargo");
+        cmd.env("RUST_BACKTRACE", "full");
+        cmd.current_dir(&node_path);
+
+        cmd.arg("run");
+        cmd.arg("--release");
+        cmd.arg("--quiet");
+        cmd.arg("--");
+        cmd.arg("--no-bootstrap 1");
+        cmd.arg(format!("--id {:?}", i));
+        cmd.arg(format!("--config-dir baker-{:?}", i));
+        cmd.arg(format!("--data-dir baker-{:?}", i));
+
+        let baker_credentials = genesis_root
+            .join(format!("bakers/baker-{}-credentials.json", i))
+            .canonicalize()
+            .context("Invalid baker credentials")?;
+
+        cmd.arg(format!(
+            "--baker-credentials-file {}",
+            baker_credentials.to_str().unwrap()
+        ));
+        cmd.arg(format!("--rpc-server-port {:?}", i + cfg.rpc_port_offset));
+        cmd.arg(format!("--listen-port {:?}", i + cfg.peer_port_offset));
+        cmd.arg("--listen-address 0.0.0.0");
+        cmd.arg(format!("--haskell-rts-flags {:?}", cfg.rts_flags));
+        cmd.arg(format!(
+            "--housekeeping-interval {:?}",
+            cfg.housekeeping_interval
+        ));
+        cmd.stdout(Stdio::piped());
+        cmd.stderr(Stdio::piped());
+
+        // every node is connected to eachother
+        if cfg.optimal_connected {
+            cmd.arg(format!("--desired-nodes {:?}", cfg.num_nodes - 1));
+            for n in 0..cfg.num_nodes {
+                if i == n {
+                    continue;
+                }
+                cmd.arg(format!(
+                    "--connect-to 127.0.0.1:{:?}",
+                    n + cfg.peer_port_offset
+                ));
+            }
+        } else {
+            // the nodes will be connected sequentially.
+            // O - O - O - O - ...
+            cmd.arg(format!("--desired-nodes {:?}", 1));
+            cmd.arg(format!("--max-allowed-nodes {:?}", 1));
+            let next_peer_port = if i == cfg.num_nodes {
+                cfg.num_nodes - 1
+            } else {
+                i + 1
+            };
+            cmd.arg(format!("--connect-to 127.0.0.1:{:?}", next_peer_port));
+        }
+
+        let mut fork = cmd
             .spawn()
             .context(format!("Failed to launch node {:?}", i))?;
 
         let mut buf_reader = BufReader::new(fork.stderr.take().context("Could not take stdout")?);
+        forks.push(fork);
         // create a channel for reading stdout of the forked process.
         let (sender, receiver) = tokio::sync::mpsc::channel(100);
         stdout_receivers.push(receiver);
@@ -121,7 +238,11 @@ fn run_app<B: Backend>(
                 for _ in 0..10 {
                     buf_reader.read_line(&mut buffered_line).unwrap();
                 }
-                sender.send(buffered_line).await.unwrap();
+                sender
+                    .send(buffered_line)
+                    .await
+                    .context("mpsc sender failed")
+                    .unwrap();
             }
         };
         tokio::spawn(reader);
@@ -131,7 +252,12 @@ fn run_app<B: Backend>(
     loop {
         // append to the logs
         for i in 0..cfg.num_nodes {
-            if let Ok(log) = stdout_receivers.get_mut(i).unwrap().try_recv() {
+            if let Ok(log) = stdout_receivers
+                .get_mut(i)
+                .context("could not get mpsc reader")
+                .unwrap()
+                .try_recv()
+            {
                 log_buffers.get_mut(i).unwrap().push_str(&log);
             };
         }
@@ -140,7 +266,9 @@ fn run_app<B: Backend>(
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Char('q') => {
-                    // todo: kill the underlying process both when pressing `q` and when sigint
+                    for mut f in forks {
+                        f.kill()?;
+                    }
                     return Ok(());
                 }
                 KeyCode::Right => app.next(),
